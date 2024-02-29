7df1f9de7226d3710bef3af30b2d44e98805b145
Philip Swiezak <philipswiezak@gmail.com>
2024-02-29 19:42:25 +0100

    add challenges

---
+++ js-basics_dark-mode/js/index.js
@@ -0,0 +1,24 @@
+console.clear();
+
+const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkButton.addEventListener('click', () => {
+  if (!bodyElement.classList.contains('dark')) {
+    bodyElement.classList.add('dark');
+  }
+})
+
+lightButton.addEventListener('click', () => {
+  if (bodyElement.classList.contains('dark')) {
+    bodyElement.classList.remove('dark');
+  }
+})
+
+toggleButton.addEventListener('click', () => {
+  bodyElement.classList.toggle('dark');
+})
+
