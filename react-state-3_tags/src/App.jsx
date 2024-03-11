5163bba45f3503a6a11414420af7f51ae3e60cfa
philfreshman <philipswiezak@gmail.com>
2024-03-11 15:59:34 +0100

    do challenge

--- react-state-3_tags/src/App.jsx
+++ react-state-3_tags/src/App.jsx
@@ -6,10 +6,13 @@ import "./App.css";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  const handleAddTag = (newTag) => setTags([...tags, newTag])
+  const handleDeleteTag = (tagToDelete) => setTags(tags.filter(tag => tag !== tagToDelete))
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag}/>
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
